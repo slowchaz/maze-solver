69bec53c67db76bf8c3223d475219a286164301f
chaz <corfitzgerald@gmail.com>
2024-04-12 10:15:50 -0500

    drawing lines

--- graphics.py
+++ graphics.py
@@ -21,3 +21,20 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+    
+    def draw(self, canvas, fill):
+        canvas.create_line(self.__start.x, self.__start.y, self.__end.x, self.__end.y, fill=fill, width=2)
+        canvas.pack(fill=BOTH, expand=1)
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     print("Hello!")
     win = Window(800, 600)
+
+    point_start = Point(0, 0)
+    point_end = Point(150, 150)
+
+    line = Line(point_start, point_end)
+
+    win.draw_line(line, "teal")
+
+    point_start = Point(150, 150)
+    point_end = Point(0, 300)
+
+    line = Line(point_start, point_end)
+
+    win.draw_line(line, "teal")
+
     win.wait_for_close()
     print("Goodbye!")
-    
+
 main()
