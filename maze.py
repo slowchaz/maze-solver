c1e36973ac11b0b86783facd496186882d92a524
chaz <corfitzgerald@gmail.com>
2024-04-12 16:05:17 -0500

    maze solving DFS solution

--- maze.py
+++ maze.py
@@ -1,5 +1,5 @@
 from graphics import Cell
-import time
+import time, random
 
 class Maze:
   def __init__(
@@ -33,6 +33,8 @@ class Maze:
         for j in range(self._num_rows):
             self._draw_cell(i, j)
     self._break_entrance_and_exit()
+    self._break_walls_r(0, 0)
+    self._reset_visited()
 
   def _draw_cell(self, i, j):
     if self._win is None:
@@ -55,3 +57,86 @@ class Maze:
 
     self._cells[self._num_cols - 1][self._num_rows - 1].break_wall(1)
     self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+
+  def _break_walls_r(self, i, j):
+    self._cells[i][j].visited = True
+
+    while True:
+      to_visit = []
+
+      if i > 0 and not self._cells[i - 1][j].visited:
+          to_visit.append((i - 1, j, 0))  # North
+      if j > 0 and not self._cells[i][j - 1].visited:
+          to_visit.append((i, j - 1, 2))  # West
+      if i < self._num_cols - 1 and not self._cells[i + 1][j].visited:
+          to_visit.append((i + 1, j, 1))  # South
+      if j < self._num_rows - 1 and not self._cells[i][j + 1].visited:
+          to_visit.append((i, j + 1, 3))  # East
+
+      if not to_visit:
+          self._draw_cell(i, j)
+          break
+
+      ni, nj, direction = random.choice(to_visit) 
+
+      # Break the wall between (i, j) and (ni, nj)
+      if direction == 0:  # North
+          self._cells[i][j].break_wall(0)
+          self._cells[ni][nj].break_wall(1)
+      elif direction == 1:  # South
+          self._cells[i][j].break_wall(1)
+          self._cells[ni][nj].break_wall(0)
+      elif direction == 2:  # West
+          self._cells[i][j].break_wall(2)
+          self._cells[ni][nj].break_wall(3)
+      else:  # East
+          self._cells[i][j].break_wall(3)
+          self._cells[ni][nj].break_wall(2)
+
+      self._draw_cell(i, j)
+      self._break_walls_r(ni, nj)
+
+  def _reset_visited(self):
+    for i in range(self._num_cols):
+        for j in range(self._num_rows):
+            self._cells[i][j].visited = False
+
+  def solve(self):
+    return self._solve_r(0, 0)
+
+  def _solve_r(self, i, j):
+    self._animate()
+    self._cells[i][j].visited = True
+
+    if (self._cells[i][j] == self._cells[self._num_cols - 1][self._num_rows - 1]):
+        return True
+
+    # for each direction
+    for direction in range(4):
+        ni, nj = i, j  
+
+        if direction == 0 and j > 0: # North
+            ni, nj = i, j - 1
+        elif direction == 1 and j < self._num_rows - 1: # South
+            ni, nj = i, j + 1
+        elif direction == 2 and i > 0: # West
+            ni, nj = i - 1, j
+        elif direction == 3 and i < self._num_cols - 1: # East
+            ni, nj = i + 1, j
+        else:
+            continue  # Skip this iteration if out of bounds
+
+        # Check if the cell can be moved into
+        if not self._cells[ni][nj].visited and (direction == 0 and not self._cells[ni][nj].has_bottom_wall or
+                                                direction == 1 and not self._cells[ni][nj].has_top_wall or
+                                                direction == 2 and not self._cells[ni][nj].has_right_wall or
+                                                direction == 3 and not self._cells[ni][nj].has_left_wall):
+            self._cells[i][j].draw_move(self._cells[ni][nj])
+            if self._solve_r(ni, nj):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[ni][nj], undo=True)
+
+    return False  # This should only be executed if all directions have been tried and failed
+          
+
--- tests.py
+++ tests.py
@@ -38,6 +38,17 @@ class Tests(unittest.TestCase):
         self.assertFalse(m1._cells[0][0].has_left_wall)
         self.assertFalse(m1._cells[num_cols - 1][num_rows - 1].has_right_wall)
 
+    def test_maze_reset_cells_visted(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in m1._cells:
+            for cell in col:
+                self.assertEqual(
+                    cell.visited,
+                    False,
+                )
+
 
 if __name__ == "__main__":
     unittest.main()
